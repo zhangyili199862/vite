a994dc58c8006a3b317265f7dfee886f4e7f77fe
zhangyili <zhangyili@supcon.com>
2021-09-08 14:48:28 +0800

    fix

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { defineComponent } from '@vue/runtime-core'
+// import "@styles/index.css"
+import '@styles/test.less'
+import classes from '@styles/test.module.css'
+
+import { a } from './test'
+import logo from './assets/logo.png'
+export default defineComponent({
+  setup() {
+    return () => {
+      return (
+        <div class={`root ${classes.moduleClass}`}>
+          hello {a.name}
+          <img src={logo}></img>
+        </div>
+      )
+    }
+  },
+})
