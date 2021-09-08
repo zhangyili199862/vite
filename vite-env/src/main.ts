7e302fc8b22a0f5ee9350f672be74e845e2b8166
zhangyili <zhangyili@supcon.com>
2021-09-08 16:39:37 +0800

    fix

---
+++ vite-env/src/main.ts
@@ -0,0 +1,8 @@
+import "./style.css";
+console.log(import.meta.env);
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
---
+++ vite-vue3/src/test.ts
@@ -0,0 +1,6 @@
+import { A } from './types'
+
+export const a: A = {
+  name: 'Jocky',
+  age: 18,
+}
