cf66134cf6ea09b5ea7104d40e4c5070ec111f30
zhangyili <zhangyili@supcon.com>
2021-09-10 13:49:37 +0800

    fix

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { throttle } from "lodash-es";
+
+console.log(throttle);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
