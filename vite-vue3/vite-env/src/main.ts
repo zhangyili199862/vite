340241fa01b4a0121d223f0dd685286ef36cf1de
zhangyili <zhangyili@supcon.com>
2021-09-10 14:48:02 +0800

    fix

--- vite-vue3/vite-env/src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-console.log(import.meta.env);
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
